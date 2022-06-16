2c629b0eae6cf473632b8955207c30dfabc3fdb5
taufik17 <taufikagungsantoso17@gmail.com>
2022-06-16 13:10:31 +0700

    navbar menit 1:11:11

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+// Hamburger
+const hamburger = document.querySelector('#hamburger')
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active')
+})
+
+// Navbar Fixed
